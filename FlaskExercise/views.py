a7560972fd0d7f9d433a41cb8db559686889f326
Surya Mishra <surya_mishra@carmax.com>
2020-12-12 05:47:07 -0500

    pushing tthis to web

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -30,11 +30,13 @@ def logout():
     if session.get('user'): # Used MS Login
         # Wipe out user and its token cache from session
         session.clear()
+        return redirect(
+        Config.AUTHORITY + '/oauth2/v2.0/logout' + '?post_logout_redirect_uri=' + url_for('login', _external=True))
         # TODO: Also logout from your tenant's web session
         #   And make sure to redirect from there back to the login page
-        pass
+  
 
-    return redirect(url_for('login'))
+    return redirect(url_for('https://login.live.com/oauth20_desktop.srf'))
 
 
 @app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
@@ -47,7 +49,12 @@ def authorized():
         cache = _load_cache()
         # TODO: Acquire a token by authorization code from an MSAL app
         #  And replace the error dictionary
-        result = {'error': 'Not Implemented', 'error_description': 'Function not implemented.'}
+        #result = {'0.ASgA-YHkPZGkd0Ce112vm8XmQqA_sD0iYpJNhMb1RQQV3UAoAPs.AQABAAIAAAB2UyzwtQEKR7-rWbgdcBZIaACG0RNVfZwFOAMFpbQjFLkMNUYLYeWmkilnnZo7qvhEnaV7AFPaoDaSNZaYWEcW7kP4Za-COiVCJ9m33_phJWBOGF-H3-bAduAfM3PPKa10yOD55uYrjA9C4DjWuSThtpGk5x-5JI302_FvtX_G3042uKcBzdAP6aJnL40AdN7hcT45x_kX1W04vW12nNkSZgMN2z1d9di5fIC95thVOMuqBQhYwS6q6bk0wfxMwNywQczxZ_Pohq_0HXfK2zhwdejCDKY2OOfXp1_C8wm_0bTcr-PPDmlsp0xQpPgnzBkkb6Aox_VQqVwFXkGUlsJ_LtVM3xXMjq81A-eYj6-WjI49pwyeijcov3tyrUx3bc48cC_Ph_ERMFQThMlSj9dQrqLwJn2j_jW7kqoPqnVgqHq7MdGIOz3jctZTXpg0y5JzP-e3nCFriMD1b1vsDkfJ0zf2yggLBrHRJTupoRTER8jkh5ZfWGdKzuN_uf3Ld2a32g0qzkfjAx1dsGh6Oijj-amc1SxqNQJS4Y4OcBuS2P4VkLHHLpjeAdAuhqKnyB0R8_wYT-5isJM3DTLjDa0SRbhNAAF85k3DQaA8gpPMIPyq8rpHy767js9DkWdwz6L1LyUDsb3_B_aZsQXM3c-nJepV9uwEh_l4AhIe3sm6Y8QXzmVJemgFkRI63iOUt-5C4UyrqHYlkx0wpsXAQhRruFQchhZCuOVkcs_Mzy53CTHv56J-6WDN4o_X3cExhrZ60ekGbiG5QQe5bA27YJ8J0sJyE-fHYQgg_QTzcEAI5u6lY7Y8_vsacrCBrrrUFSH5C6apgonyHYlGfgADU8U0MFaYsXf6rZRqvcvxniSsRiRb7UpjCzjfTdwUfWVLIYraPLlrnsmDFJ3i_6GQZ3rH_YHPZcW5HO5XUby6SSZ0lE3-WWD0HHbmqlFL5MKCFSz2IpBMYIuOdRGjORRLiQPNqowK1oG7_6HVGHl3UM8uSYH-PA6byAW_0QTm3nh33HGwB0_hmxboLCGD8Q8wxTGjhrftzXWaisI0N12lMyNNnB50CdTGr17GAC_g4jdAR2FWRSyD2sM8rf-LDO4uXdwEmFMpBHm26vFJm2muqqEUkqelHvaTHQDpR7WKBM6Fbpc211JnKHNEJoCk5omM1JH6dUY8K8IFjUyoWNcpaJ_C3-aTIxHpYs-L0LEjxKy4fiyMnvTERdUA8kqCKsoHK34_ve9e04VV11sVKjQkiDxFJzL-uNU69_A1H4n3t7NkmjksH3fknjT-McfEzw-BULbpIAA&state=4986ea9e-2360-4238-94a6-83c83601699c&session_state=26acfbab-4453-40a4-a07d-ec3a13171e2f': 'Not Implemented', 'error_description': 'Function not implemented.'}
+        result=_build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args['code'],
+        scopes=Config.SCOPE,
+        redirect_uri=url_for('authorized',_external=True,_scehme='https'))
+        #redirect_uri=url_for('authorized',_external=True))
+        
         if 'error' in result:
             return render_template('auth_error.html', result=result)
         session['user'] = result.get('id_token_claims')
@@ -74,10 +81,14 @@ def _save_cache(cache):
 
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Create and return a Confidential Client Application from msal
-    return None
-
-
+     return msal.ConfidentialClientApplication(
+     Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
+     client_credential=Config.CLIENT_SECRET, token_cache=cache)
+    
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Get the authorization request URL from a built msal app, and return it
-    return None
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+    scopes or [],
+    state=state or str(uuid.uuid4()),
+    redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+    #redirect_uri=url_for('authorized', _external=True))
+    
--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 class Config(object):
     # In a production app, store this instead in KeyVault or an environment variable
     # TODO: Enter your client secret from Azure AD below
-    CLIENT_SECRET = "{YOUR-CLIENT-SECRET}" 
+    CLIENT_SECRET = "Dq08S._vC_~4_2BUcKLaT9xAOa3U.j.V5U" 
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+    #AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
+    AUTHORITY = "https://login.microsoftonline.com/3de481f9-a491-4077-9ed7-5daf9bc5e642"
 
     # TODO: Enter your application client ID here
-    CLIENT_ID = "{YOUR-APP-CLIENT-ID}"
+    CLIENT_ID = "3db03fa0-6222-4d92-84c6-f5450415dd40"
 
     # TODO: Enter the redirect path you want to use for OAuth requests
     #   Note that this will be the end of the URI entered back in Azure AD
-    REDIRECT_PATH = "/{YOUR-REDIRECT-PATH}"  # Used to form an absolute URL, 
+    REDIRECT_PATH = "/cmsproject"  # Used to form an absolute URL, 
         # which must match your app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
